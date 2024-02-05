a062b9a70df05eaadd56dbf86c3c59e1724b5030
Amrita-k1640 <amritakashyap1998@gmail.com>
2024-02-05 13:37:02 +0530

    jenkins setup

---
+++ src/com/java/codingprepartion/Main.java
@@ -0,0 +1,11 @@
+package com.java.codingprepartion;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is for testing of Java with jenkin pipeline.");
+
+	}
+
+}
